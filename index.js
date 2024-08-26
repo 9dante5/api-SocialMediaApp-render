605a47a158a0e530a200d53719f1b1ef0e38fef3
unknown <andresep.30@gmail.com>
2024-08-26 09:39:45 -0500

    actualizado

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 30001;
+const port = process.env.PORT || 5004;
 
 server.use(middlewares);
-server.use(router);
-server.listen(port);
+server.use(router)
+server.listen(port)
